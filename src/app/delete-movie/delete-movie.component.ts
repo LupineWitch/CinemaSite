b1e5bfd2895c140e6759916b5e22c172e5a11312
Kinga L <memoey99@gmail.com>
2021-01-06 20:23:55 +0100

    validate if deleted movie is used in seance

--- src/app/delete-movie/delete-movie.component.ts
+++ src/app/delete-movie/delete-movie.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AppService } from '../app.service';
 import { Movie } from '../movie';
+import { Seance } from '../seance';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-delete-movie',
@@ -9,13 +12,21 @@ import { Movie } from '../movie';
 export class DeleteMovieComponent implements OnInit {
   @Input() selected: Movie;
   @Output() deleteMovie: EventEmitter<number> = new EventEmitter();
+  seanses: Seance[];
 
-  constructor() { }
+  constructor(private appService: AppService) { }
 
   ngOnInit(): void {
   }
 
   delete(): void {
+    this.appService.getSeanses().subscribe((seansesList: Seance[]) => {
+      this.seanses = seansesList;
+      console.log(seansesList);
+    });
+    if (this.seanses.find(x => x.movie.id == this.selected.id)){
+      Swal.fire('Ostrzeżenie!', 'Nie można usunąć filmu, dla którego istnieją seanse', 'warning');
+    }
     console.log(this.selected);
     this.deleteMovie.emit(this.selected.id);
   }
